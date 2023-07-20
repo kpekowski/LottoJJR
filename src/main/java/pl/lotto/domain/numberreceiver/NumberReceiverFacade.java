bc151c8ba7ac1b86004a741d261ad6763f971861
kuba <kuba200213@gmail.com>
2023-07-20 18:14:31 +0200

    implemeted dto and number validation with unit tests

--- src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,5 +1,19 @@
 package pl.lotto.domain.numberreceiver;
 
 
+import lombok.AllArgsConstructor;
+import pl.lotto.domain.numberreceiver.dto.InputNumberResultsDto;
+
+import java.util.Set;
+
+@AllArgsConstructor
 public class NumberReceiverFacade {
+    private NumberValidator validator;
+
+    public InputNumberResultsDto input(Set<Integer> numbersFromUser) {
+        boolean areAllNumbersInRange = validator.areAllNumbersInRange(numbersFromUser);
+        if (areAllNumbersInRange)
+            return InputNumberResultsDto.builder().message("success").build();
+        return InputNumberResultsDto.builder().message("failed").build();
+    }
 }
---
+++ src/main/java/pl/lotto/domain/numberreceiver/NumberValidator.java
@@ -0,0 +1,9 @@
+package pl.lotto.domain.numberreceiver;
+
+import java.util.Set;
+
+class NumberValidator {
+    boolean areAllNumbersInRange(Set<Integer> numbersFromUser) {
+        return numbersFromUser.stream().filter(number -> number <= 99).filter(number -> number >= 1).count() == 6;
+    }
+}
